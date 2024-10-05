1228bd512ba5a31344b411329a850905e741688d
Alexander_721 <alexander3post@gmail.com>
2024-10-05 14:01:43 +0300

    version 1.0.0

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import TheHeader from './components/common/TheHeader.vue'
-import AppBase from './components/AppBase.vue'
-</script>
-
-<template>
-    <TheHeader />
-    <AppBase />
-</template>
