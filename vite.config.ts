18ccaf3136451f636242831991a6895a48f0ebbe
Alexander_721 <alexander3post@gmail.com>
2024-11-27 14:44:34 +0300

    build test

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/V-Notes/',
     plugins: [
         vue({
             template: { transformAssetUrls }
